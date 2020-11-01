e9abce33aaba975ed6bfcb4eba1efbd2bfe51fc0
daniilafendulov <daniilafendulov@bk.ru>
2020-11-01 18:22:30 +0300

    Replace a bunch of parameters with an array

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LEScheck
 {
@@ -10,36 +6,37 @@ namespace LEScheck
     {
         static void Main(string[] args)
         {
-            Func<double, double, double, double, double>[] funcs = new Func<double, double, double, double, double>[]
+            Func<double[],double>[] funcs = new Func<double[], double>[]
             {
-                (x1,x2,x3,x4)=>7 * x1 - 4 * x2 - 4 * x3 + x4,
-                (x1,x2,x3,x4)=>5 * x1 - 4 * x3 - x4,
-                (x1,x2,x3,x4)=>-1 * x1 + 2 * x2 - x4
+                (x)=>   x[0]     +x[1]   -x[2]   -x[3],
+                (x)=>   x[0]   -2*x[1]   -x[2]           -x[4],
+                (x)=> 2*x[0]     -x[1]           +x[3]   +x[4]
             };
 
-            // базисные переменные выраженные через свободные
-            double bv1(double x1, double x2) => (-5.0 ) - (-5.0 / 2 * x1) - (3.0 / 2 * x2);
-            double bv2(double x1, double x2) => (-3.0 ) - (-2.0 * x1) - (1.0  * x2);
 
             // свободные переменные
             double fv1 = 1;
             double fv2 = 2;
 
+            // базисные переменные выраженные через свободные
+            double bv1 = (2.0) - (1.0 * fv1) - (-2.0 * fv2);
+            double bv2 = (2.0) - (-2.0 * fv1) - (1.0 * fv2);
+            double bv3 = (5.0) - (1.0 * fv1) - (1.0 * fv2);
             // подстановка переменных 
-            double[] variables = new double[] { bv2(fv1, fv2), fv1, bv1(fv1, fv2), fv2 };
+            double[] variables = new double[] { fv1,fv2,bv2,bv1,bv3};
 
             // ответный столбец
-            double[] ans = new double[] { -1,5,3 };
+            double[] ans = new double[] { -4,-7, 7};
 
             LESCheck(funcs, variables, ans);
             Console.ReadLine();
         }
 
-        static void LESCheck(Func<double, double, double, double, double>[] funcs, double[] roots, double[] ans)
+        static void LESCheck(Func<double[], double>[] funcs, double[] roots, double[] ans)
         {
             for (int i = 0; i < funcs.Length; i++)
             {
-                Console.WriteLine(funcs[i](roots[0], roots[1], roots[2], roots[3]) + "\t" + ans[i]);
+                Console.WriteLine(funcs[i](roots) + "\t" + ans[i]);
             }
         }
     }
